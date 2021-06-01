189c9b57a508ab584e5e6f3c1e7546087efc5a37
Laurenz Grote <laurenzfg@laurenzgrote.de>
2021-06-01 17:14:45 +0200

    Material UI and Sign In Stub

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { createMuiTheme } from '@material-ui/core/styles';
+import { AppBar, Container, CssBaseline, makeStyles, ThemeProvider, Toolbar, Typography } from '@material-ui/core';
+import SignIn from './Components/SignIn';
+
+// Created with material.io: https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=283593&secondary.color=1E88E5&primary.text.color=ffffff
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#5f5fc4',
+      main: '#283593',
+      dark: '#001064',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#6ab7ff',
+      main: '#1e88e5',
+      dark: '#005cb2',
+      contrastText: '#000',
+    },
+  },
+});
+
+const useStyles = makeStyles((theme: any) => ({
+  appBarSpacer: theme.mixins.toolbar,
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <AppBar position="absolute">
+          <Toolbar>
+            <Typography component="h1" variant="h6" color="inherit" noWrap>
+              Ourslides
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <div className={classes.appBarSpacer} />
+          <Container maxWidth="lg">
+            <SignIn />
+          </Container>
+        </main>
+      </ThemeProvider>
     </div>
   );
 }
