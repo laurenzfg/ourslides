63b1bca9b1bf94c39465d872f0b5a9b3a72f00a4
Laurenz Grote <laurenzfg@laurenzgrote.de>
2021-06-06 12:10:24 +0200

    Spinner with margin top

--- src/features/components/Spinner.tsx
+++ src/features/components/Spinner.tsx
@@ -2,7 +2,7 @@ import { CircularProgress, Box } from '@material-ui/core';
 
 export default function Spinner () {
   return (
-    <Box justifyContent="center" alignItems="center" display="flex" minHeight="75vh">
+    <Box justifyContent="center" alignItems="center" display="flex" mt="40vh">
       <CircularProgress />
     </Box>
   );
